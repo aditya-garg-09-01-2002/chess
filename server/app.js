1e9e1646faafaed1515106e8e6fbd4572e8c0040
mynameisaditya824@gmail.com <aditya-garg-09-01-2002>
2024-01-26 20:25:45 +0530

    transmitting chance flag to the sockets

--- server/app.js
+++ server/app.js
@@ -17,5 +17,12 @@ io.on('connection',socket=>{
         players[socket.id]={isChance:Object.keys(players).length===0}
     else 
         audience.push(socket.id)
+    socket.on('mychance',()=>{
+        console.log(socket.id,players[socket.id].isChance)
+        if(players[socket.id].isChance)
+            socket.emit('your-chance-true')
+        else 
+            socket.emit('your-chance-false')
+    })
     socket.on('disconnect',()=>delete players[socket.id])
 })

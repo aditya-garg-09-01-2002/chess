4a324e3a2999c27900f7d791339c82e04cf47fcc
mynameisaditya824@gmail.com <aditya-garg-09-01-2002>
2024-01-28 17:21:07 +0530

    check and check mate function created

---
+++ client/src/utils/check.js
@@ -0,0 +1,177 @@
+import { getPossibleMovesKing } from "./moves";
+
+function inCheck({index,board,to,piece})
+{
+    board[to].isOccupied=true;
+    board[to].color="black";
+    board[to].piece=piece;
+    if(Math.floor(index/8)!=0)
+    {
+        if(inCheckByPawn({index,board}))
+            return true;
+        if(inCheckByRook({index,board}))
+            return true;
+        if(inCheckByKnight({index,board}))
+            return true;
+        if(inCheckByBishop({index,board}))
+            return true;
+        if(inCheckByQueen({index,board}))
+            return true;
+    }
+    return false;
+}
+function inCheckByPawn({index,board})
+{
+    for(let i=-1;i<2;i++)
+    {
+        const tile=board[index-8+i];
+        if(tile.isOccupied===true&&tile.color==="black"&&tile.piece==="pawn")
+            return true;
+    }
+    return false;
+}
+function inCheckByKnight({index,board})
+{
+    const knightAt=[];
+    let row=Math.floor(index/8)
+    let col=index%8;
+    for(let i=1;i<=2;i++)
+        for(let j=1;j<=2;j++)
+        {
+            if(i+j===3)
+            {
+                if(row+i<8)
+                {
+                    if(col+j<8)
+                        knightAt.push((row+i)*8+col+j);
+                    if(col-j>-1)
+                        knightAt.push((row+i)*8+col-j);
+                }
+                if(row-i>-1)
+                {
+                    if(col+j<8)
+                        knightAt.push((row-i)*8+col+j);
+                    if(col-j>-1)
+                        knightAt.push((row-i)*8+col-j)
+                }
+            }
+        }
+    for(let i=0;i<knightAt.length;i++)
+        if(board[knightAt[i]].isOccupied===true&&board[knightAt[i]].color==="black"&&board[knightAt[i]].piece==="knight")
+            return true;
+    return false;
+}
+function inCheckByRook({index,board})
+{
+    let rookInSight=false;
+    let flag10=true,flag20=true,flag30=true,flag40=true;
+    for(let i=1;i<8;i++)
+    {
+        if(index-i>-1)
+        {
+            if(board[index-i].isOccupied===true)
+            {
+                if(board[index-i].color==="black"&&board[index-i].piece==="rook"&&flag10===true)
+                    rookInSight=true;
+                else 
+                    flag10=false;
+            }
+        }
+        if(index+i<8*Math.floor(index/8)+8)
+        {
+            if(board[index+i].isOccupied===true)
+            {
+                if(board[index+i].color==="black"&&board[index+i].piece==="rook"&&flag20===true)
+                    rookInSight=true;
+                else 
+                    flag20=false;
+            }
+        }
+        if(index-8*i>-1)
+        {
+            if(board[index-8*i].isOccupied===true)
+            {
+                if(board[index-8*i].color==="black"&&board[index-8*i].piece==="rook"&&flag30===true)
+                    rookInSight=true
+                else   
+                    flag30=false;
+            }
+        }
+        if(index+8*i<64)
+        {
+            if(board[index+8*i].isOccupied===true)
+            {
+                if(board[index+8*i].color==="black"&&board[index+8*i].piece==="rook"&flag40===true)
+                    rookInSight=true;
+                else 
+                    flag40=false;
+            }
+        }
+    }
+    return rookInSight;
+}
+function inCheckByBishop({index,board})
+{
+    let bishopInSight=false;
+    let flag1=true,flag2=true,flag3=true,flag4=true;
+    for(let i=1;i<8;i++)
+    {
+        if(index-7*i>-1)
+        {
+            if(board[index-7*i].isOccupied===true)
+            {
+                if(board[index-7*i].color==="black"&&board[index-7*i].piece==="bishop"&&flag1===true)
+                    bishopInSight=true;
+                else
+                    flag1=false;
+            }
+        }
+        if(index-9*i>-1)
+        {            
+            if(board[index-9*i].isOccupied===true)
+            {
+                if(board[index-9*i].color==="black"&&board[index-9*i].piece==="bishop"&&flag2===true)
+                    bishopInSight=true;
+                else 
+                    flag2=false;
+            }
+        }
+        if(index+7*i<64)
+        {
+            if(board[index+7*i].isOccupied===true)
+            {
+                if(board[index+7*i].color==="black"&&board[index+7*i].piece==="bishop"&&flag3===true)
+                    bishopInSight=true;
+                else 
+                    flag3=false;
+            }
+        }
+        if(index+9*i<64)
+        {
+            if(board[index+9*i].isOccupied===true)
+            {
+                if(board[index+9*i].color==="black"&&board[index+9*i].piece==="bishop"&&flag4===true)
+                    bishopInSight=true;
+                else 
+                    flag4=false;
+            }
+        }
+    }
+    return bishopInSight
+}
+function inCheckByQueen({index,board})
+{
+    return (inCheckByBishop({index,board})||inCheckByRook({index,board}))
+}
+function inCheckMate({index,board,to,piece})
+{
+    board[to].isOccupied=true;
+    board[to].color="black";
+    board[to].piece=piece;
+    const possibleMoves=getPossibleMovesKing(index,board);
+    for(let i=0;i<possibleMoves.length;i++)
+        if(inCheck({index:possibleMoves[i],board,to,piece})===false)
+            return false;
+    return true;
+}
+export {inCheck,inCheckMate}

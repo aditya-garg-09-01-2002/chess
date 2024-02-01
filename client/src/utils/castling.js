512c6ee4ae3fa1e090f2b8346d65e3272fbb5d62
mynameisaditya824@gmail.com <aditya-garg-09-01-2002>
2024-02-01 19:46:38 +0530

    updated check castling hooks

--- client/src/utils/castling.js
+++ client/src/utils/castling.js
@@ -2,37 +2,33 @@ import { inCheck } from "./check";
 
 function checkKingSideCastling({board,from,to,piece})
 {
+    let allow=false;
     if(board[60].moved===false&&board[63].moved==false)
     {
         if(board[61].isOccupied===false&&board[62].isOccupied===false)
         {
             board[60].isOccupied=false;
             if(inCheck({index:61,board,from,to,piece})===false&&inCheck({index:62,board,from,to,piece})===false)
-            {
-                board[60].isOccupied=true;
-                return true;
-            }
+                allow=true;
             board[60].isOccupied=true;
         }
     }
-    return false;
+    return allow;
 }
 function checkQueenSideCastling({board,from,to,piece})
 {
+    let allow=false;
     if(board[60].moved===false&&board[56].moved===false)
     {
         if(board[57].isOccupied===false&&board[58].isOccupied===false&&board[59].isOccupied===false)
         {
             board[60].isOccupied=false;
             if(inCheck({index:57,board,from,to,piece})===false&&inCheck({index:58,board,from,to,piece})===false&&inCheck({index:59,board,from,to,piece})===false)
-            {
-                board[60].isOccupied=true;
-                return true;
-            }
+                allow=true;
             board[60].isOccupied=true;
         }
     }
-    return false;
+    return allow;
 }
 
 function setCastling({setKingSideCastling,setQueenSideCastling,state,to,from,piece})

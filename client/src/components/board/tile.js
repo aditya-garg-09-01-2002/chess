3ff60ab2841058a9a68e9bad11c2346e37fbed89
mynameisaditya824@gmail.com <aditya-garg-09-01-2002>
2024-02-01 17:16:34 +0530

    formatting correction

--- client/src/components/board/tile.js
+++ client/src/components/board/tile.js
@@ -2,15 +2,15 @@ import React from "react"
 import {Pawn,Rook,King,Knight,Queen,Bishop} from "./pieces"
 
 const Tile= React.forwardRef(({isOccupied,isEven,piece,color,select,index,moveTo,isMyChance},ref)=>{
-        const pieces={
-            pawn: Pawn,
-            rook: Rook,
-            knight: Knight,
-            bishop: Bishop,
-            queen: Queen,
-            king: King,
-        };
-        const Piece=pieces[piece];
+    const pieces={
+        pawn: Pawn,
+        rook: Rook,
+        knight: Knight,
+        bishop: Bishop,
+        queen: Queen,
+        king: King,
+    };
+    const Piece=pieces[piece];
     return (
         <>
             <div ref={ref} 

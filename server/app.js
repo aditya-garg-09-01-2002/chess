a5e62ab462076329137faf74bcea552c1b5d80f0
mynameisaditya824@gmail.com <aditya-garg-09-01-2002>
2024-02-03 10:52:11 +0530

    updated pairing for a match logic

--- server/app.js
+++ server/app.js
@@ -10,58 +10,63 @@ const server=app.listen(appConfig.Port,()=>console.log('connected to server...')
 
 const io=socketIO(server,{cors:{}})
 
-let players={},audience=[];
-
+let attendees=-1;
+let pairs={};
 io.on('connection',socket=>{
-    if(Object.keys(players).length<2)
+    if(attendees==-1)
+        attendees=socket.id;
+    else
     {
-        players[socket.id]={isChance:Object.keys(players).length===0}
-        if(players[socket.id].isChance===true)
-            socket.emit('you-start')
+        const playerOne=attendees;
+        const playerTwo=socket.id;
+        attendees=-1;
+        pairs[playerOne]={opponent:playerTwo,isChance:true}
+        pairs[playerTwo]={opponent:playerOne,isChance:false}
+        io.to(playerOne).emit('you-start')
+        io.to(playerOne).emit('player-one');
+        io.to(playerTwo).emit('player-two');
     }
-    else 
-        audience.push(socket.id)
     socket.on('mychance',()=>{
-        if(typeof(players[socket.id])==='undefined')
-            socket.emit('your-chance-false')
-        else if(players[socket.id].isChance)
+        if(typeof pairs[socket.id]==='undefined')
+        {
+            socket.emit('wait for other player to join')
+            return ;
+        }
+        if(pairs[socket.id].isChance===true)
             socket.emit('your-chance-true')
         else 
             socket.emit('your-chance-false')
     })
     socket.on('have-castled',({side})=>{
-        players[socket.id].isChance=false;
-        const otherPlayerID=Object.keys(players).find(id=>id!==socket.id);
-        players[otherPlayerID].isChance=true;
+        pairs[socket.id].isChance=false;
+        const otherPlayerID=pairs[socket.id].opponent;
+        pairs[otherPlayerID].isChance=true;
         io.to(otherPlayerID).emit('castle-opponent',{side})
     })
     socket.on('check-mate',()=>{
-        const otherPlayerID=Object.keys(players).find(id=>id!==socket.id)
-        io.to(otherPlayerID).emit('you-won');
-        // console.log(socket.id,otherPlayerID)
+        io.to(pairs[socket.id].opponent).emit('you-won');
     })
     socket.on('have-moved',({from,to,piece})=>{
         socket.emit('check-status')
-        players[socket.id].isChance=false;
-        const otherPlayerID=Object.keys(players).find(id=>id!==socket.id);
-        players[otherPlayerID].isChance=true;
+        
+        pairs[socket.id].isChance=false;
+        const otherPlayerID=pairs[socket.id].opponent;
+        pairs[otherPlayerID].isChance=true;
         // Converting index from my perspective to opponent perspective
         io.to(otherPlayerID).emit('opponent-move',{to:56-to+2*(to%8),from:56-from+2*(from%8),piece})
     })
     socket.on('promote-pawn',({index,piece})=>{
-        const otherPlayerID=Object.keys(players).find(id=>id!==socket.id);
-        io.to(otherPlayerID).emit('pawn-promotion',{index:56-index+2*(index%8),piece})
+        io.to(pairs[socket.id].opponent).emit('pawn-promotion',{index:56-index+2*(index%8),piece})
     })
     socket.on('disconnect',()=>{
-        try{
-            const otherPlayerID=Object.keys(players).find(id=>id!==socket.id);
-            players[otherPlayerID].isChance=true;
-            socket.to(otherPlayerID).emit('you-start')
-        }
-        catch(error)
-        {
-            console.log('other player left as well')
-        }
-        delete players[socket.id]
+        if(attendees===socket.id)
+            attendees=-1;
+        if(typeof pairs[socket.id]!=='undefined')
+            if(typeof pairs[socket.id].opponent!=='undefined')
+            {
+                io.to(pairs[socket.id].opponent).emit('other-player-left');
+                delete pairs[pairs[socket.id].opponent];
+                delete pairs[socket.id];
+            }
     })
 })

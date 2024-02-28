89dee07b675c8cc3539b739058b9a24d0de5e6de
mynameisaditya824@gmail.com <aditya-garg-09-01-2002>
2024-02-28 12:31:07 +0530

    added provider, store and removed isChance and setChance

--- client/src/App.js
+++ client/src/App.js
@@ -4,55 +4,59 @@ import Panel from "./components/panel/panel.js"
 import NotifyPrompt from './components/notifyPrompt/notifyPrompt.js';
 import { useState } from 'react';
 import {useMediaQuery} from "react-responsive"
+import {store} from "./redux/store.js"
+import {Provider} from "react-redux"
+
 function App() {
   const isMobile=useMediaQuery({query:'(max-width:768px)'});
   const isPortrait=useMediaQuery({query:'(orientation:portrait)'});
   const [otherPlayerPresent,setOtherPlayerPresent]=useState(false);
-  const [otherPlayerLeft,setOtherPlayerLeft]=useState(false);
+  const [otherPlayerLeft,setOtherPlayerLeft]=useState(false); 
   const isMobilePortrait=isMobile&&isPortrait
   const [kingSideCastling,setKingSideCastling]=useState(false)
   const [queenSideCastling,setQueenSideCastling]=useState(false)
   const [isCheck,setCheck]=useState(false)
   const [isCheckMate,setCheckMate]=useState(false)
-  const [isChance,setChance]=useState(false)
   const [doKingSideCastling,setDoKingSideCastling]=useState(false)
   const [doQueenSideCastling,setDoQueenSideCastling]=useState(false)
   const [winner,setWinner]=useState(false)
+
   return (
-    <div id={
-      isMobilePortrait?
+    <Provider store={store}>
+      <div id={
+        isMobilePortrait?
         "main-mobile":
         "main"
-    }>               
-      {
-        otherPlayerPresent===false?
+      }>               
+        {
+          otherPlayerPresent===false?
           <NotifyPrompt type="waiting-for-opponent"/>:""
-      }              
-      {
-        otherPlayerLeft?
-          <NotifyPrompt type="opponent-left"/>:""
-      }
-      <Board 
-        setOtherPlayerLeft={(value)=>setOtherPlayerLeft(value)}
-        setOtherPlayerPresent={(value)=>setOtherPlayerPresent(value)}
-        isMobilePortrait={isMobilePortrait}
-        setWinner={value=>setWinner(value)}
-        setChance={value=>setChance(value)} 
-        setKingSideCastling={value=>setKingSideCastling(value)} 
-        setQueenSideCastling={value=>setQueenSideCastling(value)} 
-        setCheck={value=>setCheck(value)} 
-        setCheckMate={value=>setCheckMate(value)}
-        doKingSideCastling={doKingSideCastling}
-        doQueenSideCastling={doQueenSideCastling}
-        setDoKingSideCastling={setDoKingSideCastling}
-        setDoQueenSideCastling={setDoQueenSideCastling}
-      />
-      {
-        otherPlayerPresent&&otherPlayerLeft===false?
-          <Panel isMobilePortrait={isMobilePortrait} isWinner={winner} isChance={isChance} kingSideCastling={kingSideCastling} queenSideCastling={queenSideCastling} isCheck={isCheck} isCheckMate={isCheckMate} setDoKingSideCastling={setDoKingSideCastling} setDoQueenSideCastling={setDoQueenSideCastling}/>
+        }              
+        {
+          otherPlayerLeft?
+            <NotifyPrompt type="opponent-left"/>:""
+        }
+        <Board 
+          setOtherPlayerLeft={(value)=>setOtherPlayerLeft(value)}
+          setOtherPlayerPresent={(value)=>setOtherPlayerPresent(value)}
+          isMobilePortrait={isMobilePortrait}
+          setWinner={value=>setWinner(value)}
+          setKingSideCastling={value=>setKingSideCastling(value)} 
+          setQueenSideCastling={value=>setQueenSideCastling(value)} 
+          setCheck={value=>setCheck(value)} 
+          setCheckMate={value=>setCheckMate(value)}
+          doKingSideCastling={doKingSideCastling}
+          doQueenSideCastling={doQueenSideCastling}
+          setDoKingSideCastling={setDoKingSideCastling}
+          setDoQueenSideCastling={setDoQueenSideCastling}
+          />
+        {
+          otherPlayerPresent&&otherPlayerLeft===false?
+          <Panel isMobilePortrait={isMobilePortrait} isWinner={winner} kingSideCastling={kingSideCastling} queenSideCastling={queenSideCastling} isCheck={isCheck} isCheckMate={isCheckMate} setDoKingSideCastling={setDoKingSideCastling} setDoQueenSideCastling={setDoQueenSideCastling}/>
           :""
-      }
-    </div >
+        }
+      </div >
+    </Provider>
   );
 }
 

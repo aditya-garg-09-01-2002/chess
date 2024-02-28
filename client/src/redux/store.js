308ed28311728f8084f78fe609c7128f0da5837d
mynameisaditya824@gmail.com <aditya-garg-09-01-2002>
2024-02-28 17:52:28 +0530

    added checkSlice

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,8 +1,10 @@
 import {configureStore} from "@reduxjs/toolkit"
 import chanceSlice from "./slices/chance"
+import checkSlice from "./slices/check"
 
 export const store=configureStore({
     reducer:{
         chance:chanceSlice,
+        check:checkSlice,
     },
 })
